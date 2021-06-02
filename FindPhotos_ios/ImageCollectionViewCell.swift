58df2e89c8473c8a13200f53449a4e076cf59468
Gabriel Nicol <moraesnicol@gmail.com>
2021-06-01 22:06:07 -0300

    finish

---
+++ FindPhotos_ios/ImageCollectionViewCell.swift
@@ -0,0 +1,53 @@
+//
+//  ImageCollectionViewCell.swift
+//  FindPhotos_ios
+//
+//  Created by Gabriel on 01/06/21.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    static let identifier = "imageCollectionViewCell"
+    
+    private let imageView: UIImageView = {
+       let imageView = UIImageView()
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(imageView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        imageView.frame = contentView.bounds
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        imageView.image = nil
+    }
+    
+    func configure(with urlString: String) {
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            DispatchQueue.main.async {
+                let image = UIImage(data: data)
+                self?.imageView.image = image
+            }
+        }.resume()
+    }
+}
